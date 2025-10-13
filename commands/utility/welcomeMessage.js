0f0daa5eac55874fc3684daf98098ca9205892da
bernardoup31 <up202108842@up.pt>
2025-10-13 15:48:47 +0100

    Github actions started

--- commands/utility/welcomeMessage.js
+++ commands/utility/welcomeMessage.js
@@ -3,6 +3,6 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder().setName('lima').setDescription('Replies something!'),
 	async execute(interaction) {
-		await interaction.reply('is gay!');
+		await interaction.reply('is gay and corno and burro!');
 	},
 };
