6e5fc6f3a4d527afe74502fd73d1d0d020640414
bernardoup31 <up202108842@up.pt>
2025-10-13 01:46:51 +0100

    Slash commands are now available

---
+++ commands/utility/welcomeMessage.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('lima').setDescription('Replies something!'),
+	async execute(interaction) {
+		await interaction.reply('is gay!');
+	},
+};
